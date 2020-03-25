7496ff482d7cd5b7db729af76e732f46e1d278e9
Piyush Ranjan <piyushranjan95@gmail.com>
2020-03-25 14:41:00 +0530

    Add dynamic center mapping

--- js/main.js
+++ js/main.js
@@ -4,15 +4,28 @@ function onLoad() {
     const titleText = document.querySelector('#title-text')
     const detail = document.querySelector('#detail')
     const submitBtn = document.querySelector('#submit-btn')
+    const citySelect = document.querySelector('#cityName')
+    const centerSelect = document.querySelector('#centerName')
 
+    
     const WELCOME_TEXT = "Join Upay"
     const THANKS_TEXT = "Thank You!"
     const WELCOME_DETAIL = "We are glad to see your interest towards helping children from underprivileged and marginalized sections of the society. Kindly provide more details about you."
     const THANKS_DETAIL = "Thank you for your interest in volunteering for Upay. We will get in touch with you soon. In the mean time you can visit our <a href=\"https://www.upay.org.in\">website</a> to know more about us or follow us on <a href=\"https://www.facebook.com/upayngo/\">facebook</a>."
     const ERROR_TEXT = "Oops"
     const ERROR_DETAIL = "We faced some problem in submitting your details. Please try again. If the issue persists please reach out to us at  infoupay@gmail.com"
+    const CITY_CENTER_MAP = {
+        "Pune": ["Gosavi Basti", "Dandekar Pul"],
+        "Mouda": ["Mathni", "Kumbhari", "Rahadi", "Isapur", "Dahali", "Tiger chowk", "Gurdev Chowk", "Lapka", "Nath Nagar", "Wadoda"],
+        "Nagpur": ["Mount Road- Sadar", "Sakkardara Square", "Wardhaman Nagar", "Sitaburdi", "IT Park", "Laxmi Nagar", "Jagdish Nagar-Katol Road", "Santra market"],
+        "Bangalore": ["Electronic city-Phase 1", "Kormangala"],
+        "Delhi": ["Karol Bagh", "Tilak Nagar"],
+        "Noida": ["Sector 18"],
+        "Gurgaon": ["Sohna Road", "Sikanderpur - Phase 2", "Sector 57", "Scottish, Sec-57"]
+    }
     const SERVICE_URL = "https://volreg.herokuapp.com"
-    
+    // const SERVICE_URL = "http://localhost:8080"
+
     titleText.textContent = WELCOME_TEXT
     detail.innerHTML = WELCOME_DETAIL
     
@@ -20,6 +33,15 @@ function onLoad() {
         e.preventDefault()
         disableForm()
         sendData()
+    })
+
+    // initialize city names 
+    initializeDropdown(citySelect, Object.keys(CITY_CENTER_MAP))
+
+    // initialize center name based on selected city name
+    citySelect.addEventListener('change', e => {
+        selectedCity = citySelect.options[citySelect.selectedIndex].text
+        initializeDropdown(centerSelect, CITY_CENTER_MAP[selectedCity])
     })
 
     function sendData() {
@@ -78,6 +100,24 @@ function disableTags(...tagNames) {
     tagNames.forEach(tagName => Array.from(document.getElementsByTagName(tagName)).forEach(elem => elem.disabled = true))
 }
 
+// utility function to populate select with options
+function initializeDropdown(dropdown, values) {
+    clearSelect(dropdown)
+    values.forEach(value => {
+        var option = document.createElement("option")
+        option.text = value
+        dropdown.add(option)
+    })
+}
+
+// utility function to clear all options of a select element
+function clearSelect(select) {
+    var i = select.options.length;
+    while(i > 0) {
+        select.remove(i);
+        i--;
+    }
+}
 
 
 
